fc403d254f391d4e201a6959072288bc798d9f03
abhinavaman <114594854+abhinavaman@users.noreply.github.com>
2024-06-03 13:50:16 +0000

    done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+//alert("3 + 3") // we are using nodejs, not browser
+
+console.log (3 + 3)
+
+console.log("Aman")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bignit
+//string =>""
+//bollean => true/false
+//null =>standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null);// output= object
+console.log(typeof undefined);//output = undefined
