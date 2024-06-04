db92cc0897b163346bc1ee1d14d90102a87f8815
abhinavaman <114594854+abhinavaman@users.noreply.github.com>
2024-06-04 07:05:12 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = 33
+
+//there are two type of methods to check type
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+// "33abc"=> NaN
+// true => 1; False => 0
+
+let isloggedIn = 1
+
+let booleanIsLoggedIn = boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 =>false
+// "" => false
+// "hitesh" =>true
+
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
