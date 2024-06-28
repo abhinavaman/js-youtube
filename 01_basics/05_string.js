1d2b1ccb620056aecd79865d48d28cb850ea5b17
abhinavaman <114594854+abhinavaman@users.noreply.github.com>
2024-06-28 07:20:33 +0000

    done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,25 @@
+const name = "aman"
+const repocount = 50
+
+console.log(`hello my name is ${name} and my repo cont is ${repocount}`);
+
+const gameName = new String('aman')
+
+//console.log(gameName[0]);
+//console.log(gameName._proto_);
+
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('n'));
+
+let newString = gameName.substring(0,2)
+console.log(newString);
+
+let anotherString = gameName.slice(0,3)
+console.log(anotherString);
+
+
+
